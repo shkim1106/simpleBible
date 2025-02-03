4d5535a7fd8b481c1f581d9593f52e8941caaff7
SuHyuk <shkim0411064@naver.com>
2025-02-03 19:02:50 +0900

    묵상 페이지 (UI), 웰컴페이지 오늘의 말씀, 테스트 페이지

---
+++ simpleBible/WelcomePage.swift
@@ -0,0 +1,17 @@
+//
+//  SearchView.swift
+//  simpleBible
+//
+//  Created by shkim on 2/2/25.
+//
+
+import SwiftUI
+
+struct WelcomePage: View {
+    var body: some View {
+        NavigationView {
+            Text("Hello")
+        }
+        .navigationTitle("환영합니다!")
+    }
+}
