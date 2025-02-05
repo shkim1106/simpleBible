83bbb12d4c8d64f247c6b27f0d985c4b8cded802
SuHyuk <shkim0411064@naver.com>
2025-02-06 00:40:05 +0900

    로그인 구체화, 사용자 프로필 및 struct 생성

--- simpleBible/ContentView.swift
+++ simpleBible/ContentView.swift
@@ -17,14 +17,49 @@ struct ContentView: View {
     @State private var profileName: String = "로그인 이름"
     
     let logInStatusInfo: (Bool) -> String = { isLoggedIn in
-        return isLoggedIn ? "로그인 성공" : "로그아웃 완료"
+        return isLoggedIn ? "로그인 성공" : "로그인이 필요합니다"
     }
     
     var body: some View {
         VStack {
             Text(logInStatusInfo(kakaoAuthVM.isLoggedIn))
+            Text(kakaoAuthVM.userProfile.nickName)
+                .font(.title.bold())
                 .padding()
-            Text(kakaoAuthVM.nickName)
+            if let url = kakaoAuthVM.userProfile.profileImageUrl {
+                            AsyncImage(url: url) { phase in
+                                switch phase {
+                                case .empty:
+                                    ProgressView() // Show loading indicator while the image is loading
+                                case .success(let image):
+                                    image
+                                        .resizable()
+                                        .aspectRatio(contentMode: .fill)
+                                        .frame(width: 100, height: 100) // Circle size
+                                        .clipShape(Circle())
+                                        .shadow(radius: 5) // Optional: Add shadow effect
+                                case .failure:
+                                    Image(systemName: "person.crop.circle.fill")
+                                        .resizable()
+                                        .aspectRatio(contentMode: .fit)
+                                        .frame(width: 100, height: 100)
+                                        .foregroundColor(.gray)
+                                @unknown default:
+                                    Image(systemName: "person.crop.circle.fill")
+                                        .resizable()
+                                        .aspectRatio(contentMode: .fit)
+                                        .frame(width: 100, height: 100)
+                                        .foregroundColor(.gray)
+                                }
+                            }
+                        } else {
+                            Image(systemName: "person.crop.circle.fill")
+                                .resizable()
+                                .aspectRatio(contentMode: .fit)
+                                .frame(width: 100, height: 100)
+                                .foregroundColor(.gray)
+                        }
+            
             Button("카카오 로그인", action: {
                 kakaoAuthVM.kakaoLogin()
             })
